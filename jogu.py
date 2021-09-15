6d7542b3ba6caf0a8c45111295b736cc6398108a
joel <joel.gustafsson@chasacademy.se>
2021-09-15 15:36:41 +0200

    changed strings,fixed a small error in the calculations with the wrong variable

--- jogu.py
+++ jogu.py
@@ -2,6 +2,6 @@ import math #imports math functions
 lengthcube = float(input("What is the length of the cube?")) #put in the length of the cube
 lengthtetra = float(input("What is the length of the tetrahedron?")) #put in the length of tetrahedron
 volumecube = lengthcube**3 #multiplies the cubes lenght by itself three times
-volumetetra = lengthcube**3/6*math.sqrt(2) #multiples the tetrahedrons lenght by 3, divdes it by 6 and then take the sqaure root of 2
-print("The cubes volume is",volumecube,"cm3") #prints the cubes volume
-print("The tetrahedrons volume is",volumetetra,"cm3") #prints the tetrahedrons volume
+volumetetra = lengthtetra**3/6*math.sqrt(2) #multiples the tetrahedrons lenght by 3, divdes it by 6 and then take the sqaure root of 2
+print("\nThe volume of the cube is",volumecube,"cm3") #prints the cubes volume
+print("\nThe tetrahedrons volume is",volumetetra,"cm3") #prints the tetrahedrons volume
