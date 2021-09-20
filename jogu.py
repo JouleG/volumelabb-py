b7e4169e28edb71828a1e577e62d337540504e10
Your Name <you@example.com>
2021-09-20 11:35:35 +0200

    removed space between lines

--- jogu.py
+++ jogu.py
@@ -2,11 +2,9 @@ import math #imports math functions
 
 # option = int(input("1: Cube \n2: Tetrahedron"))
 # while True:
-#     if option == 1 or option == 2:
-#         break
-    
-
-#if option == 1:
+#      if option == 1 or option == 2:
+#          break
+# if option == 1:
 try:
     lengthcube = float(input("What is the length of the cube?")) #put in the length of the cube
 
@@ -22,17 +20,17 @@ except ValueError:
 
 #if option == 2:
 try:
-    lengthtetra = float(input("What is the length of the tetrahedron?")) #put in the length of tetrahedron
+        lengthtetra = float(input("What is the length of the tetrahedron?")) #put in the length of tetrahedron
 
 except ValueError:
-        while True:
-            try:
-                lengthtetra = float(input("Must be a valid number"))
+            while True:
+                try:
+                    lengthtetra = float(input("Must be a valid number"))
 
-            except ValueError:
-                pass
-            else:
-                break
+                except ValueError:
+                    pass
+                else:
+                    break
 
 volumecube = lengthcube**3 #multiplies the cubes lenght by itself three times
 volumetetra = lengthtetra**3/6*math.sqrt(2) #multiples the tetrahedrons lenght by 3, divdes it by 6 and then take the sqaure root of 2
